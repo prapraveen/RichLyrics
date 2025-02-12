0d368c6bf219d8a52d393675058bae248839011f
prapraveen <praveencubes@gmail.com>
2025-02-12 13:17:18 -0500

    switched to react vite project

---
+++ spotify-code/src/App.jsx
@@ -0,0 +1,41 @@
+import './App.css'
+import { useEffect, useState } from 'react';
+
+function App() {
+  const clientId = "cb52476ef4c5422abfd747fb1bd8b701";
+  const params = new URLSearchParams(window.location.search);
+  const code = params.get("code");
+  const [access_code, setAccess_code] = useState(null)
+
+  useEffect(() => {
+    if (!code) {
+      redirectToAuthCodeFlow(clientId);
+    } else {
+        setAccess_code(code)
+        // const accessToken = await getAccessToken(clientId, code);
+        // displayAccessToken(accessToken);
+        // await sendAccessToken(accessToken);
+    }
+  }, [])
+  
+
+    
+  return <>
+    <h1>Your Spotify Code:</h1>
+    <h2>{access_code}</h2>
+    </>
+}
+
+export default App
+
+
+async function redirectToAuthCodeFlow(clientId) {
+
+  const params = new URLSearchParams();
+  params.append("client_id", clientId);
+  params.append("response_type", "code");
+  params.append("redirect_uri", "https://rich-lyrics.vercel.app/callback");
+  params.append("scope", "user-read-playback-state");
+
+  document.location = `https://accounts.spotify.com/authorize?${params.toString()}`;
+}
