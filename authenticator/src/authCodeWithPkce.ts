4334dd0654f6a36c0cad2eb5e19b2317ec178faf
prapraveen <praveencubes@gmail.com>
2025-02-12 11:34:09 -0500

    changed urls

--- authenticator/src/authCodeWithPkce.ts
+++ authenticator/src/authCodeWithPkce.ts
@@ -3,7 +3,7 @@ export async function redirectToAuthCodeFlow(clientId: string) {
     const params = new URLSearchParams();
     params.append("client_id", clientId);
     params.append("response_type", "code");
-    params.append("redirect_uri", "http://localhost:5173/callback");
+    params.append("redirect_uri", "https://prapraveen.github.io/RichLyrics/callback");
     params.append("scope", "user-read-playback-state");
 
     document.location = `https://accounts.spotify.com/authorize?${params.toString()}`;
@@ -16,7 +16,7 @@ export async function getAccessToken(clientId: string, code: string) {
     params.append("client_id", clientId);
     params.append("grant_type", "authorization_code");
     params.append("code", code);
-    params.append("redirect_uri", "http://localhost:5173/callback");
+    params.append("redirect_uri", "https://prapraveen.github.io/RichLyrics/callback");
     params.append("code_verifier", verifier!);
 
     const result = await fetch("https://accounts.spotify.com/api/token", {
@@ -27,23 +27,4 @@ export async function getAccessToken(clientId: string, code: string) {
 
     const { access_token } = await result.json();
     return access_token;
-}
-
-function generateCodeVerifier(length: number) {
-    let text = '';
-    let possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-
-    for (let i = 0; i < length; i++) {
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
-    }
-    return text;
-}
-
-async function generateCodeChallenge(codeVerifier: string) {
-    const data = new TextEncoder().encode(codeVerifier);
-    const digest = await window.crypto.subtle.digest('SHA-256', data);
-    return btoa(String.fromCharCode.apply(null, [...new Uint8Array(digest)]))
-        .replace(/\+/g, '-')
-        .replace(/\//g, '_')
-        .replace(/=+$/, '');
 }
