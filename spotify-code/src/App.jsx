e127b8c0f045da839198d1d617e1a0c1a5776b06
prapraveen <praveencubes@gmail.com>
2025-02-12 13:40:17 -0500

    updated redirect url

--- spotify-code/src/App.jsx
+++ spotify-code/src/App.jsx
@@ -34,7 +34,7 @@ async function redirectToAuthCodeFlow(clientId) {
   const params = new URLSearchParams();
   params.append("client_id", clientId);
   params.append("response_type", "code");
-  params.append("redirect_uri", "http://localhost:5173/callback");
+  params.append("redirect_uri", "https://rich-lyrics.vercel.app/");
   params.append("scope", "user-read-playback-state");
 
   document.location = `https://accounts.spotify.com/authorize?${params.toString()}`;
